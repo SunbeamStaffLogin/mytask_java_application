ea569dfcb6ad46e86e0193729f993aee9b7ed209
rugvedak <rugvedak1994@gmail.com>
2024-02-23 11:11:26 +0530

    U,R,S,C,CS model created.

---
+++ spring_boot_backend_template/src/main/java/com/app/entities/Course.java
@@ -0,0 +1,23 @@
+package com.app.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "course")
+public class Course extends BaseEntity {
+	@Column
+	private String courseName;
+	@Column
+	private LocalDate duration;
+}
